1e7aa74eff536bbbde55f1b4d0c5c44f3bf1f658
Stanislau Matskevich <st.matskevich.work@gmail.com>
2020-05-08 13:42:28 +0300

    Removed excess solution

--- src/main/scala/lab1.scala
+++ src/main/scala/lab1.scala
@@ -4,34 +4,9 @@ import scala.annotation.tailrec
 
 object Lab1 {
   def main(args: Array[String]) {
-    val words = List[String]("test", "big", "boy", "largest")
-    println(getMaxRec(words))
-    println(getMaxRecTail(words))
-
     val num = 9
     println(decToBin(num))
     println(decToBinTail(num))
-  }
-
-  def getMaxRec(words: List[String]): Int = {
-        def search(words: List[String], idx: Int): Int = {
-            idx match {
-                case 0 => words(idx).length
-                case _ => words(idx).length.max(search(words, idx - 1))
-            }
-        }
-        search(words, words.length - 1)
-  }
-
-  def getMaxRecTail(words: List[String]): Int = {
-        @tailrec
-        def search(words: List[String], idx: Int, best: Int): Int = {
-            idx match {
-                case 0 => words(idx).length.max(best)
-                case _ => search(words, idx - 1, words(idx).length.max(best))
-            }
-        }
-        search(words, words.length - 1, 0)
   }
 
   def decToBin(num: Int): String = {
--- src/test/scala/lab1.test.scala
+++ src/test/scala/lab1.test.scala
@@ -3,21 +3,6 @@ import org.scalatest._
 
 
 class Lab1Tests extends FunSuite {
-    test("max equal max(tailrec)") {
-        val words = List[String]("test", "big", "boy", "largest")
-        assert(Lab1.getMaxRec(words) === Lab1.getMaxRecTail(words))
-    }
-
-    test("return max length") {
-        val words = List[String]("test", "big", "boy", "largest")
-        assert(Lab1.getMaxRec(words) === 7)
-    }
-
-    test("return max length(tailrec)") {
-        val words = List[String]("test", "big", "boy", "largest")
-        assert(Lab1.getMaxRecTail(words) === 7)
-    }
-
     test("decToBin equal decToBin(tailrec)") {
         val num = 259
         assert(Lab1.decToBin(num) === Lab1.decToBinTail(num))
